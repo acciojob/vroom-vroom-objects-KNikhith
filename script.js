be03a19da9fa98288ad4f0e84874c5df862c7e93
KNikhith <nikhithkotluri@gmail.com>
2026-03-25 22:00:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+Car.prototype.getMakeModel=()=>{
+	return `${this.make} ${this.model}`
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topspeed=topspeed; 
+}
+SportsCar.prototype=Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar;
+SportsCar.prototype.getTopSpeed=()=>{
+	return this.topspeed;
+}
 
 // Do not change the code below
 window.Car = Car;
